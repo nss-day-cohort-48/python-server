f2ef30abb89b0b9bb1406b3b8da8385e9bd62be0
Hannah Hall <hannahkhall13@gmail.com>
2021-07-19 13:55:30 -0500

    ch 8-9

--- animals/request.py
+++ animals/request.py
@@ -1,3 +1,8 @@
+import sqlite3
+import json
+
+from models import Animal
+
 ANIMALS = [
     {
         "id": 1,
@@ -32,7 +37,31 @@ def get_all_animals():
     Returns:
         [List]: list of dictionaries
     """
-    return ANIMALS
+    with sqlite3.connect('./kennel.db') as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        select
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            
+        from animal a
+        """)
+
+        dataset = db_cursor.fetchall()
+        animals = []
+
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+            animals.append(animal.__dict__)
+
+    return json.dumps(animals)
+
 
 def get_single_animal(id):
     """Gets a single animal from the list
@@ -43,12 +72,35 @@ def get_single_animal(id):
     Returns:
         [dictionary]: The selected animal
     """
-    requested_animal = None
-    for animal in ANIMALS:
-        if animal['id'] == id:
-            requested_animal = animal
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    return requested_animal
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.id = ?
+        or a.name = ?
+        """, (id, "snickers"))
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        animal = Animal(data['id'], data['name'], data['breed'],
+                        data['status'], data['location_id'],
+                        data['customer_id'])
+
+        return json.dumps(animal.__dict__)
+
 
 def create_animal(animal):
     max_id = ANIMALS[-1]['id']
@@ -59,6 +111,7 @@ def create_animal(animal):
     ANIMALS.append(animal)
 
     return animal
+
 
 def delete_animal(id):
     """
@@ -77,6 +130,7 @@ def delete_animal(id):
     if animal_index >= 0:
         ANIMALS.pop(animal_index)
 
+
 def update_animal(id_of_animal, new_animal_dict):
     """
         [summary]
@@ -94,4 +148,3 @@ def update_animal(id_of_animal, new_animal_dict):
             # new animal dict arg
             ANIMALS[index] = new_animal_dict
             break
-        
